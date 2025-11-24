f2d834b5aacd0d21b6c905d82716e1a25752daa4
Joseph Kamaka <josephkamata09@gmail.com>
2025-11-24 07:31:22 -1000

    Fix build issue

--- src/app/authentication/verify-email/page.tsx
+++ src/app/authentication/verify-email/page.tsx
@@ -1,5 +1,8 @@
 import { VerifyEmailClient } from './_components/VerifyEmailClient'
 
+// Force dynamic rendering - this page requires search params
+export const dynamic = 'force-dynamic'
+
 export default function VerifyEmailPage() {
   return <VerifyEmailClient />
 }
